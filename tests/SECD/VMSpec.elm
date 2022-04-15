3955f358de06723a8ad311b2240a670bc1e65944
Joshua Ji <joshua.ji@jiwhiz.com>
2022-04-15 15:12:39 -0600

    if/else tests

--- tests/SECD/VMSpec.elm
+++ tests/SECD/VMSpec.elm
@@ -1,9 +1,9 @@
 module SECD.VMSpec exposing (..)
 
-import Expect
+import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer)
-import SECD.Program as Prog exposing (Func(..), Op(..))
-import SECD.VM as VM
+import SECD.Program as Prog exposing (Func(..), Op(..), Program)
+import SECD.VM as VM exposing (Value)
 import Test exposing (Test)
 
 
@@ -12,8 +12,7 @@ suite =
     Test.describe "Test VM"
         [ testBasics
         , testBuiltIns
-
-        -- , testIfElse
+        , testIfElse
         ]
 
 
@@ -26,14 +25,14 @@ testBasics =
                     program =
                         Prog.fromSingleton NIL
                 in
-                Expect.equal (VM.evaluate <| VM.init program) (Ok VM.Nil)
+                vmExpectSuccess program VM.Nil
         , Test.test "Returns Constant in a LDC program" <|
             \_ ->
                 let
                     program =
                         Prog.fromSingleton (LDC 5)
                 in
-                Expect.equal (VM.evaluate <| VM.init program) (Ok <| VM.Integer 5)
+                vmExpectSuccess program (VM.Integer 5)
         ]
 
 
@@ -50,37 +49,67 @@ testBuiltIns =
                     program =
                         Prog.fromList [ LDC a, LDC b, Func Add ]
                 in
-                Expect.equal (VM.evaluate <| VM.init program) (Ok <| VM.Integer (a + b))
+                vmExpectSuccess program (VM.Integer (a + b))
         , Test.fuzz fuzzIntPair "Multiplies two numbers correctly" <|
             \( a, b ) ->
                 let
                     program =
                         Prog.fromList [ LDC a, LDC b, Func Mult ]
                 in
-                Expect.equal (VM.evaluate <| VM.init program) (Ok <| VM.Integer (a * b))
+                vmExpectSuccess program (VM.Integer (a * b))
         , Test.test "Addition fails when types are wrong" <|
             \_ ->
                 let
                     program =
                         Prog.fromList [ NIL, LDC 5, Func Add ]
                 in
-                Expect.err (VM.evaluate <| VM.init program)
+                vmExpectFailure program
         , Test.test "Atom function correctly identifies a number as an atom" <|
             \_ ->
                 let
                     program =
                         Prog.fromList [ LDC 5, Func Atom ]
                 in
-                Expect.equal (VM.evaluate <| VM.init program) (Ok <| VM.Boolean True)
+                vmExpectSuccess program (VM.Boolean True)
+        , Test.test "Atom function fails on empty stack" <|
+            \_ ->
+                let
+                    program =
+                        Prog.fromList [ Func Atom ]
+                in
+                vmExpectFailure program
         ]
 
 
 
 -- if/else control flow
--- testIfElse : Test
--- testIfElse =
---     Test.describe "Tests if/else control flow"
---         []
+
+
+testIfElse : Test
+testIfElse =
+    Test.describe "Tests if/else control flow"
+        [ Test.test "Correctly chooses the left side" <|
+            \_ ->
+                let
+                    program =
+                        Prog.fromList [ LDC 5, Func Atom, SEL, Nested [ LDC 1, JOIN ], Nested [ LDC 2, JOIN ] ]
+                in
+                vmExpectSuccess program (VM.Integer 1)
+        ]
+
+
+
+-- runs the VM on the program, and expect the result to succeed and be the expected value
+
+
+vmExpectSuccess : Program -> Value -> Expectation
+vmExpectSuccess prog expected =
+    Expect.equal (VM.evaluate <| VM.init prog) (Ok expected)
+
+
+vmExpectFailure : Program -> Expectation
+vmExpectFailure prog =
+    Expect.err (VM.evaluate <| VM.init prog)
 
 
 fuzzIntPair : Fuzzer ( Int, Int )
