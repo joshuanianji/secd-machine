9cfaed2901fcaaa312f8f7d3d641296b099adfe7
Joshua Ji <joshua.ji@jiwhiz.com>
2022-05-29 20:28:40 -0600

    test: :white_check_mark: Add tests for `Views.Compiled`
    The logic behind this is kinda simple, so tests were just to solidify that it was working
    Althoughhhhh, my tests weren't that broad. Maybe I should add some more...

--- src/Views/Compiled.elm
+++ src/Views/Compiled.elm
@@ -1,9 +1,13 @@
 module Views.Compiled exposing
     ( APType(..)
     , Code(..)
+    , Indexed(..)
     , Model
     , Msg
+    , Unindexed(..)
+    , getIndices
     , init
+    , stripIndices
     , subscriptions
     , transpile
     , update
@@ -17,6 +21,7 @@ import Browser.Navigation exposing (Key)
 import Element exposing (Element)
 import Element.Background as Background
 import Element.Border as Border
+import Element.Font as Font
 import Element.Input as Input
 import Html
 import Html.Attributes
@@ -47,7 +52,7 @@ type alias Model =
 
 
 type alias OkModel =
-    { code : List Code }
+    { code : List Indexed }
 
 
 init : Program -> ( Model, Cmd Msg )
@@ -63,71 +68,221 @@ init prog =
 -- convert the Program code to our internal representation, useful for UI stuff
 
 
-transpile : List Prog.Op -> Result Error (List Code)
-transpile ops =
+transpile : List Prog.Op -> Result Error (List Indexed)
+transpile =
+    transpile_ 0 >> Result.map Tuple.second
+
+
+
+-- transpile helper function alsp keeps track of a "counter" to uniquely identify each code node
+
+
+transpile_ : Int -> List Prog.Op -> Result Error ( Int, List Indexed )
+transpile_ n ops =
     case ops of
         Prog.NIL :: xs ->
-            Result.map ((::) NIL) <| transpile xs
+            transpile_ (n + 1) xs
+                |> Result.map (Tuple.mapSecond <| \rest -> Indexed ( n, NIL ) :: rest)
 
         (Prog.LD coords) :: xs ->
-            Result.map ((::) <| LD coords) <| transpile xs
+            transpile_ (n + 1) xs
+                |> Result.map (Tuple.mapSecond <| \rest -> Indexed ( n, LD coords ) :: rest)
 
         (Prog.LDC x) :: xs ->
-            Result.map ((::) <| LDC x) <| transpile xs
+            transpile_ (n + 1) xs
+                |> Result.map (Tuple.mapSecond <| \rest -> Indexed ( n, LDC x ) :: rest)
 
         Prog.LDF :: (Prog.FUNCBODY name _) :: xs ->
-            Result.map ((::) <| LDFunc name) <| transpile xs
+            transpile_ (n + 1) xs
+                |> Result.map (Tuple.mapSecond <| \rest -> Indexed ( n, LDFunc name ) :: rest)
 
         Prog.LDF :: (Prog.NESTED nested) :: Prog.AP :: xs ->
-            Result.map2 (\nest rest -> LDApply nest AP :: rest) (transpile nested) (transpile xs)
+            transpile_ (n + 1) nested
+                |> Result.map (Tuple.mapSecond <| \nest -> Indexed ( n, LDApply AP nest ))
+                |> Result.andThen
+                    (\( m, code ) ->
+                        transpile_ m xs
+                            |> Result.map (Tuple.mapSecond <| \rest -> code :: rest)
+                    )
 
         Prog.LDF :: (Prog.NESTED nested) :: Prog.RAP :: xs ->
-            Result.map2 (\nest rest -> LDApply nest RAP :: rest) (transpile nested) (transpile xs)
+            transpile_ (n + 1) nested
+                |> Result.map (Tuple.mapSecond <| \nest -> Indexed ( n, LDApply RAP nest ))
+                |> Result.andThen
+                    (\( m, code ) ->
+                        transpile_ m xs
+                            |> Result.map (Tuple.mapSecond <| \rest -> code :: rest)
+                    )
 
         Prog.LDF :: (Prog.NESTED nested) :: xs ->
-            Result.map2 (\nest rest -> LDLambda nest :: rest) (transpile nested) (transpile xs)
+            transpile_ (n + 1) nested
+                |> Result.map (Tuple.mapSecond <| \nest -> Indexed ( n, LDLambda nest ))
+                |> Result.andThen
+                    (\( m, code ) ->
+                        transpile_ m xs
+                            |> Result.map (Tuple.mapSecond <| \rest -> code :: rest)
+                    )
 
         Prog.RTN :: xs ->
-            Result.map ((::) <| RTN) <| transpile xs
+            transpile_ (n + 1) xs
+                |> Result.map (Tuple.mapSecond <| \rest -> Indexed ( n, RTN ) :: rest)
 
         Prog.SEL :: (Prog.NESTED nestedT) :: (Prog.NESTED nestedF) :: xs ->
-            Result.map3 (\nestT nestF rest -> SEL nestT nestF :: rest) (transpile nestedT) (transpile nestedF) (transpile xs)
+            transpile_ (n + 1) nestedT
+                |> Result.map (Tuple.mapSecond <| \nestT nestF -> Indexed ( n, SEL nestT nestF ))
+                |> Result.andThen
+                    (\( m, sel1 ) ->
+                        transpile_ m nestedF
+                            |> Result.map (Tuple.mapSecond <| \nestF -> sel1 nestF)
+                            |> Result.andThen
+                                (\( m1, sel2 ) ->
+                                    transpile_ m1 xs
+                                        |> Result.map (Tuple.mapSecond <| \rest -> sel2 :: rest)
+                                )
+                    )
 
         Prog.JOIN :: xs ->
-            Result.map ((::) <| JOIN) <| transpile xs
+            transpile_ (n + 1) xs
+                |> Result.map (Tuple.mapSecond <| \rest -> Indexed ( n, JOIN ) :: rest)
 
         Prog.DUM :: xs ->
-            Result.map ((::) <| DUM) <| transpile xs
+            transpile_ (n + 1) xs
+                |> Result.map (Tuple.mapSecond <| \rest -> Indexed ( n, DUM ) :: rest)
 
         (Prog.FUNC f) :: xs ->
-            Result.map ((::) <| FUNC (Prog.funcToString f)) <| transpile xs
+            transpile_ (n + 1) xs
+                |> Result.map (Tuple.mapSecond <| \rest -> Indexed ( n, FUNC <| Prog.funcToString f ) :: rest)
 
         Prog.AP :: xs ->
-            Result.map ((::) <| LoneAP) <| transpile xs
+            transpile_ (n + 1) xs
+                |> Result.map (Tuple.mapSecond <| \rest -> Indexed ( n, LoneAP ) :: rest)
 
         [] ->
-            Ok []
+            Ok ( n, [] )
 
         op :: _ ->
             Err <| "Unexpected op! " ++ Prog.opToString op
 
 
+initChain : Int -> a -> Result Error ( Int, ( Int, a ) )
+initChain n code =
+    Ok ( n, ( n, code ) )
+
+
+
+-- add a transpiled list to the end of a single indexed
+
+
+addTranspiled : List Prog.Op -> Result Error ( Int, Indexed ) -> Result Error ( Int, List Indexed )
+addTranspiled ops r =
+    case r of
+        Ok ( n, idxed ) ->
+            transpile_ (n + 1) ops
+                |> Result.map (Tuple.mapSecond <| \rest -> idxed :: rest)
+
+        Err e ->
+            Err e
+
+
+
+-- each code block will be indexed by a unique ID
+
+
+type Indexed
+    = Indexed ( Int, Code Indexed )
+
+
+
+-- an unindexed code block is just the code itself. Stripping the indices is sometimes useful for testing
+
+
+type Unindexed
+    = Unindexed (Code Unindexed)
+
+
+stripIndices : Indexed -> Unindexed
+stripIndices (Indexed ( _, code )) =
+    case code of
+        NIL ->
+            Unindexed NIL
+
+        LD coords ->
+            Unindexed (LD coords)
+
+        LDC x ->
+            Unindexed (LDC x)
+
+        LDFunc name ->
+            Unindexed (LDFunc name)
+
+        LDApply aptype nested ->
+            Unindexed (LDApply aptype (List.map stripIndices nested))
+
+        LDLambda nested ->
+            Unindexed (LDLambda (List.map stripIndices nested))
+
+        SEL nestedT nestedF ->
+            Unindexed (SEL (List.map stripIndices nestedT) (List.map stripIndices nestedF))
+
+        RTN ->
+            Unindexed RTN
+
+        JOIN ->
+            Unindexed JOIN
+
+        DUM ->
+            Unindexed DUM
+
+        FUNC f ->
+            Unindexed (FUNC f)
+
+        LoneAP ->
+            Unindexed LoneAP
+
+
+
+-- other times, we only want the indices
+
+
+getIndices : List Indexed -> List Int
+getIndices ops =
+    let
+        getIndex : Indexed -> List Int
+        getIndex (Indexed ( n, code )) =
+            case code of
+                LDApply _ nested ->
+                    n :: getIndices nested
+
+                LDLambda nested ->
+                    n :: getIndices nested
+
+                SEL nestedT nestedF ->
+                    n :: getIndices nestedT ++ getIndices nestedF
+
+                -- single elements
+                _ ->
+                    [ n ]
+    in
+    List.map getIndex ops
+        |> List.concat
+
+
 
 -- I need to beef up the program type (List Op) into something more helpful
--- Thus, I traverse the compiled ops and generate my own tree structure used just for a interactinv with the code
+-- Thus, I traverse the compiled ops and generate my own tree structure used just for a interactive with the code
 -- a lot of these changes can simply be built into the Program type itself, but I want to keep the program type as simple as possible
 
 
-type Code
+type Code a
     = NIL
     | LD ( Int, Int )
     | LDC Int
     | LDFunc String -- Loads a function name (when a function is defined in a let stmt)
-    | LDLambda (List Code) -- loads a lambda (when a lambda is an argument to a function)
-    | LDApply (List Code) APType -- loads a function and apply it
+    | LDLambda (List a) -- loads a lambda (when a lambda is an argument to a function)
+    | LDApply APType (List a) -- loads a function and apply it
     | LoneAP -- if a function is loaded from the env then applied
     | RTN
-    | SEL (List Code) (List Code)
+    | SEL (List a) (List a)
     | JOIN
     | DUM
     | FUNC String -- Builtin function (just use their string representation lol)
@@ -163,24 +318,20 @@ update msg model =
 
 view : Model -> Element Msg
 view model =
-    Element.column
-        []
-        [ Debug.toString model.original
-            |> Element.text
-            |> List.singleton
-            |> Element.paragraph []
-        , case model.stuff of
-            Err e ->
-                Element.html <| Error.view e
+    case model.stuff of
+        Err e ->
+            Element.html <| Error.view e
 
-            Ok okModel ->
-                viewOk okModel
-        ]
+        Ok okModel ->
+            viewOk okModel
 
 
 viewOk : OkModel -> Element Msg
 viewOk model =
-    Element.text "transpilation success c:"
+    Debug.toString model.code
+        |> Element.text
+        |> List.singleton
+        |> Element.paragraph [ Font.size 20 ]
 
 
 
--- tests/Views/CompiledSpec.elm
+++ tests/Views/CompiledSpec.elm
@@ -1,15 +1,21 @@
 module Views.CompiledSpec exposing (suite)
 
 import Expect
+import Lib.LispAST as AST
+import Lib.Util as Util
+import List.Extra
+import Programs
 import SECD.Program as Prog
 import Test exposing (Test)
-import Views.Compiled exposing (APType(..), Code(..), transpile)
+import Views.Compiled exposing (..)
 
 
 suite : Test
 suite =
     Test.describe "compiledSpec"
-        [ testTranspiler ]
+        [ testTranspiler
+        , testTranspilerIndices
+        ]
 
 
 testTranspiler : Test
@@ -28,7 +34,8 @@ transpilerTrivials =
                 Test.test (Prog.opToString op) <|
                     \_ ->
                         transpile [ op ]
-                            |> Expect.equal (Ok [ expected ])
+                            |> Result.map (List.map stripIndices)
+                            |> Expect.equal (Ok [ Unindexed expected ])
             )
             [ ( Prog.NIL, NIL )
             , ( Prog.LD ( 0, 0 ), LD ( 0, 0 ) )
@@ -48,22 +55,59 @@ transpilerLDF =
                 Test.test name <|
                     \_ ->
                         transpile ops
+                            |> Result.map (List.map stripIndices)
                             |> Expect.equal (Ok expected)
             )
             [ ( "LDF followed by funcbody"
               , [ Prog.LDF, Prog.FUNCBODY "sub" [ Prog.LD ( 0, 1 ), Prog.LD ( 0, 0 ), Prog.FUNC Prog.SUB, Prog.RTN ] ]
               , [ LDFunc "sub" ]
+                    |> List.map Unindexed
               )
             , ( "LDF with nested and rap"
               , [ Prog.LDF, Prog.NESTED [ Prog.LDC 1, Prog.LD ( 0, 0 ), Prog.FUNC Prog.ADD, Prog.RTN ], Prog.RAP ]
-              , [ LDApply [ LDC 1, LD ( 0, 0 ), FUNC "+", RTN ] RAP ]
+              , [ LDApply RAP (List.map Unindexed [ LDC 1, LD ( 0, 0 ), FUNC "+", RTN ]) ]
+                    |> List.map Unindexed
               )
             , ( "LDF during building args (no rap/ap)"
               , [ Prog.NIL, Prog.LDF, Prog.NESTED [ Prog.LDC 1, Prog.LD ( 0, 0 ), Prog.FUNC Prog.ADD, Prog.RTN ], Prog.FUNC Prog.CONS ]
-              , [ NIL, LDLambda [ LDC 1, LD ( 0, 0 ), FUNC "+", RTN ], FUNC "CONS" ]
+              , [ NIL, LDLambda (List.map Unindexed [ LDC 1, LD ( 0, 0 ), FUNC "+", RTN ]), FUNC "CONS" ]
+                    |> List.map Unindexed
               )
             , ( "((lambda (f) (f 1)) (lambda (x) (+ x 1)))"
               , [ Prog.NIL, Prog.LDF, Prog.NESTED [ Prog.LDC 1, Prog.LD ( 0, 0 ), Prog.FUNC Prog.ADD, Prog.RTN ], Prog.FUNC Prog.CONS, Prog.LDF, Prog.NESTED [ Prog.NIL, Prog.LDC 1, Prog.FUNC Prog.CONS, Prog.LD ( 0, 0 ), Prog.AP, Prog.RTN ], Prog.AP ]
-              , [ NIL, LDLambda [ LDC 1, LD ( 0, 0 ), FUNC "+", RTN ], FUNC "CONS", LDApply [ NIL, LDC 1, FUNC "CONS", LD ( 0, 0 ), LoneAP, RTN ] AP ]
+              , [ NIL, LDLambda (List.map Unindexed [ LDC 1, LD ( 0, 0 ), FUNC "+", RTN ]), FUNC "CONS", LDApply AP (List.map Unindexed [ NIL, LDC 1, FUNC "CONS", LD ( 0, 0 ), LoneAP, RTN ]) ]
+                    |> List.map Unindexed
               )
+            ]
+
+
+testTranspilerIndices : Test
+testTranspilerIndices =
+    Test.describe "Test transpiler for generating correct indices - assuming parser and compiler works" <|
+        List.map
+            (\( name, code ) ->
+                let
+                    indices =
+                        AST.parse code
+                            |> Result.mapError Util.deadEndsToString
+                            |> Result.andThen Prog.compile
+                            |> Result.andThen (Prog.toList >> transpile)
+                            |> Result.map getIndices
+                in
+                Test.test name <|
+                    \_ ->
+                        case indices of
+                            Ok ids ->
+                                -- ensure ids are all unique
+                                List.Extra.unique ids
+                                    |> Expect.equal ids
+
+                            Err err ->
+                                Expect.fail <| "Transpiler index failed somewhere: " ++ err
+            )
+            [ ( "simple", "((lambda (x) (+ x 1)) 1)" )
+            , ( "Mut Rec 4", Programs.mutuallyRecursiveIsEven 4 )
+            , ( "Recursive Length", Programs.recursiveLength )
+            , ( "Factorial 5", Programs.factorial 5 )
+            , ( "fib 10", Programs.fibonacci 10 )
             ]
